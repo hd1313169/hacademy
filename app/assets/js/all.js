d46af03e6edcbaf3364ec4180ed515f6eec8dcf8
Ryan Chiang <wsad71155@gmail.com>
2022-12-30 21:18:42 +0800

    1230

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -31,31 +31,26 @@ var lessonTagSwiper = new Swiper(".lessonTag-swiper", {
 
 var lessonTypeSwiper = new Swiper(".lessonType-swiper", {
   // Default parameters
-  slidesPerView: 2,
+  slidesPerView: 2.5,
   spaceBetween: 16,
   // Responsive breakpoints
   breakpoints: {
-    // when window width is >= 320px
-    576: {
-      slidesPerView: 2,
-      spaceBetween: 16
-    },
+
     // when window width is >= 480px
     768: {
-      slidesPerView: 4,
+      slidesPerView: 3.5,
       spaceBetween: 16
     },
     // when window width is >= 640px
     992: {
-      slidesPerView: 6,
+      slidesPerView: 5.5,
       spaceBetween: 16
     }
   },
 
-
-autoplay: {
-  delay: 2000,
-},
+// autoplay: {
+//   delay: 2000,
+// },
 });
 
 const elem = document.querySelector('input[name="datepicker"]');
